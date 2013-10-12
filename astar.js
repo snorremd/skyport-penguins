f96a9a239339200eb445230eba7fc63237d1e7e4
Torstein Thune <torstein@gmail.com>
2013-10-12 13:01:02 +0200

    Added priorityqueue implementation, a* shait

--- astar.js
+++ astar.js
@@ -1,26 +1,34 @@
-function path(map, start, end) {
-	
-	var openList = [],
-		closedList = [start],
-		surroundingNodes = surroundingNodes(start);
+var PriorityQueue = require('libs/priorityQueue.js');
 
-	
-
-	costSurroundingNodes()
-
-	for(var i = 0; i < surroundingNodes.length; i++) {
-
-	}
-
-
+function astar() {
 
 }
 
-function costSurroundingNodes(map, surroundingNodes, endNodes) {
+exports.astar = astar;
+
+function path(map, startNode, endNode) {
+	
+	var openList = [],
+		closedList = [startNode],
+		surroundingNodes = surroundingNodes(startNode),
+		cheapestNode,
+		lowestFScore = Number.MAX_VALUE,
+		openQueue = PriorityQueue({ low: true });
+
+	costSurroundingNodes = costSurroundingNodes(map, startNode, endNode);
+
+	for(var i = 0; i < surroundingNodes.length; i++) {
+		surroundingNodes[i].fScore = gCost(surroundingNodes[i]); + hCost(map, surroundingNodes[i], endNode);
+		openQueue.push(surroundingNodes[i], surroundingNodes[i].fScore);
+	}
+
+}
+
+function costSurroundingNodes(map, surroundingNodes, endNode) {
 	var costArray = [];
 
 	for(var i = 0;i<surroundingNodes.length;i++) {
-		costArray.push(gCost(surroundingNodes[i]) + hCost(surroundingNodes[i]));
+		costArray.push(gCost(surroundingNodes[i]) + hCost(map, surroundingNodes[i], endNode));
 	}
 
 	return costArray;
@@ -31,26 +39,63 @@ function gCost(map, adjacentNode) {
 }
 
 function hCost(map, currentNode, endNode) {
-	var cost = 0;
-	var path = []
-	var cheapestNode;
-	var surroundingNodes = surroundingNodes(currentNode);
+	var cost = 0,
+		path = [],
+		cheapestAirPathCost = Number.MAX_VALUE,
+		surroundingNodes = surroundingNodes(currentNode),
+		airPathCost;
+
 	for(var i = 0; i < surroundingNodes; i++) {
-		airPath = []
-		airDistCost = airDistCost(map, surroundingNodes, endNode, airPath);
-	}
+		airPathCost = airPathCost(map, surroundingNodes[i], endNode);
 
-
-}
-
-function airDistCost(map, startNode, endNode) {
-	airCost = 0
-	adjacentNodes = surroundingNodes(startNode);
-	for(var i=0; i < adjacentNodes.length; i++) {
-		if(airDistance(map, adjacentNode[i]) < airCost) {
-			adjacentNodes
+		if(airPathCost < cheapestAirPathCost){
+			cheapestAirPathCost = airPathCost;
 		}
 	}
+
+	return cheapestAirPathCost;
+}
+
+function recursiveAirDistance(currentNode, endNode, airPath, map) {
+
+	if(currentNode === endNode) {
+		return airPath.push(endNode);
+	}
+
+	var adjacentNodes = surroundingNodes(currentNode),
+		lowestCostNode = {
+			airDist: Number.MAX_VALUE,
+			x: 0,
+			y: 0
+		};
+
+	for(var i = 0;i < adjacentNodes.length;i++) {
+		var airDistance = airDistance(map, adjacentNodes[i], endNode);
+		if(airDistance < lowestCostNode.airDist) {
+			lowestCostNode = {
+				airDist: airDistance,
+				x: adjacentNodes.x,
+				y: adjacentNodes.y
+			}
+		}
+	}
+
+	airPath.push(lowestCostNode);
+
+	recursiveAirDistance(lowestCostNode, endNode, map);
+
+	return airPath;
+}
+
+function airPathCost(map, startNode, endNode, airPath) {
+
+	var cost = 0;
+
+	for(var i=0;i<airPath.length;i++) {
+		cost += calculateNodeCost(getNodeValue(airPath[i])); // get value of node (return node value)
+	}
+
+	return cost;
 }
 
 function airDistance(map, node, endNode) {
@@ -96,6 +141,10 @@ function surroundingNodes(start) {
 	}
 	return surroundingNodes;
 }
+
+function getNodeValue(node, map) {
+	return map[node.j][node.k];
+} 
 
 
 
